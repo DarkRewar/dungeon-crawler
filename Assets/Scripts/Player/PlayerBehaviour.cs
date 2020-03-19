c6f60ae0eb12754fc167014346330808e035abdd
DarkRewar <curtis.pelissier@laposte.net>
2020-03-19 15:02:31 +0100

    Head bobbing + smooth movement

--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -21,19 +21,22 @@ public class PlayerBehaviour : MonoBehaviour
     private bool _isRotating = false;
     private Vector3 _movement;
 
+    private Vector3 _previousPosition;
     private Vector3 _nextPosition;
+
+    private Quaternion _previousRotation;
     private Quaternion _nextRotation;
     private float _actualMoveTime = 0;
     private float _actualRotateTime = 0;
 
     private void Awake()
     {
-        GameManager.Instance.Player = this;
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        GameManager.Instance.Player = this;
         _baseCamPos = PlayerCamera.transform.localPosition;
     }
 
@@ -45,70 +48,78 @@ public class PlayerBehaviour : MonoBehaviour
             Input.GetAxis("Vertical"),
             Input.GetAxis("Pivot")
         );
-        
-        if(_movement != Vector3.zero && !_isRotating && !_isMoving)
+
+        void CheckInput()
         {
-            if(Mathf.Abs(_movement.x) > Mathf.Abs(_movement.y))
+            if (_movement != Vector3.zero && !_isRotating && !_isMoving)
             {
-                _nextPosition = transform.position + transform.right * Mathf.Sign(_movement.x);
+                _previousPosition = transform.position;
+                _previousRotation = transform.rotation;
 
-                if (!Physics.Raycast(
-                    PlayerCamera.transform.position, 
-                    transform.right * Mathf.Sign(_movement.x),
-                    1.5f
-                ))
+                if (Mathf.Abs(_movement.x) > Mathf.Abs(_movement.y))
                 {
-                    _isMoving = true;
-                    _actualMoveTime = 0;
+                    _nextPosition = transform.position + transform.right * Mathf.Sign(_movement.x);
 
-                    GameManager.OnPlayerMove?.Invoke(transform.position);
+                    if (!Physics.Raycast(
+                        PlayerCamera.transform.position,
+                        transform.right * Mathf.Sign(_movement.x),
+                        1.5f
+                    ))
+                    {
+                        _isMoving = true;
+                        _actualMoveTime = 0;
+
+                        GameManager.OnPlayerMove?.Invoke(transform.position);
+                    }
                 }
-            }
-            else if(_movement.y != 0)
-            {
-                _nextPosition = transform.position + transform.forward * Mathf.Sign(_movement.y);
-
-                if (!Physics.Raycast(
-                    PlayerCamera.transform.position,
-                    transform.forward * Mathf.Sign(_movement.y),
-                    1.1f
-                ))
+                else if (_movement.y != 0)
                 {
-                    _isMoving = true;
-                    _actualMoveTime = 0;
+                    _nextPosition = transform.position + transform.forward * Mathf.Sign(_movement.y);
 
-                    GameManager.OnPlayerMove?.Invoke(transform.position);
+                    if (!Physics.Raycast(
+                        PlayerCamera.transform.position,
+                        transform.forward * Mathf.Sign(_movement.y),
+                        1.1f
+                    ))
+                    {
+                        _isMoving = true;
+                        _actualMoveTime = 0;
+
+                        GameManager.OnPlayerMove?.Invoke(transform.position);
+                    }
                 }
-            }
-            else
-            {
-                _actualRotateTime = 0;
-                _isRotating = true;
-                _nextRotation = Quaternion.Euler(transform.eulerAngles + Vector3.up * 90 * Mathf.Sign(_movement.z));
+                else
+                {
+                    _actualRotateTime = 0;
+                    _isRotating = true;
+                    _nextRotation = Quaternion.Euler(transform.eulerAngles + Vector3.up * 90 * Mathf.Sign(_movement.z));
 
-                //GameManager.OnPlayerMove?.Invoke(transform.position);
+                    //GameManager.OnPlayerMove?.Invoke(transform.position);
+                }
             }
         }
+
+        CheckInput();
+
+        Debug.Log($"IsMoving = {_isMoving}");
 
         if (_isMoving)
         {
             _actualMoveTime += Time.deltaTime;
-            transform.position = Vector3.Lerp(transform.position, _nextPosition, MoveCurve.Evaluate(_actualMoveTime / MoveTime));
+            transform.position = Vector3.Lerp(_previousPosition, _nextPosition, MoveCurve.Evaluate(_actualMoveTime / MoveTime));
+            //transform.position = Vector3.Lerp(_previousPosition, _nextPosition, Mathf.Clamp01(_actualMoveTime / MoveTime));
 
-            //Vector3 camera = _baseCamPos;
-            //camera.y = Mathf.Lerp(
-            //    _baseCamPos.y,
-            //    _baseCamPos.y,
-            //    CameraCurve.Evaluate(_actualMoveTime / MoveTime)
-            //);
-            //PlayerCamera.transform.localPosition = camera;
+            Vector3 camera = _baseCamPos;
+            camera.y = _baseCamPos.y * CameraCurve.Evaluate(_actualMoveTime / MoveTime);
+            PlayerCamera.transform.localPosition = camera;
+            Debug.Log(camera);
 
             if (_actualMoveTime >= MoveTime) _isMoving = false;
         }
         else if (_isRotating)
         {
             _actualRotateTime += Time.deltaTime;
-            transform.rotation = Quaternion.Slerp(transform.rotation, _nextRotation, MoveCurve.Evaluate(_actualRotateTime / RotateTime));
+            transform.rotation = Quaternion.Slerp(_previousRotation, _nextRotation, MoveCurve.Evaluate(_actualRotateTime / RotateTime));
             if (_actualRotateTime >= RotateTime) _isRotating = false;
         }
     }
